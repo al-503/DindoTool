68bbf09a307441e073d7b77f2ce00eb3a7d18952
axel <axel.ridray@sfr.fr>
2025-05-08 20:55:05 +0200

    add calcul for reproduction time

--- js/vue/DragodindeCard.js
+++ js/vue/DragodindeCard.js
@@ -3,6 +3,33 @@ class DragodindeCard {
         this._dragodinde = dragodinde
     }
 
+    /**
+     * 
+     * @param {Number} generation 
+     * @returns {Number} time in ms
+     */
+    calculateGestationTime(generation) {
+        const base = 36;
+        const tranche = 12;
+        const timeInHours = base + tranche * generation
+        return convertGestationTimeInMs(timeInHours)
+    }
+
+    /**
+     * 
+     * @param {Number} gestationTime time in hours
+     * @returns {Number} time in ms
+     */
+    convertGestationTimeInMs(gestationTime) {
+        const hourInMs = 3600000;
+        return hourInMs * gestationTime;
+    }
+
+    calculateDateToGiveBirth(generation) {
+        // la date AJD + le temp de gestation en ms
+        return new Date(Date.now + calculateGestationTime(generation))
+    }
+
     createDragodindesCard() {
         const $wrapper = document.createElement('div')
         $wrapper.classList.add('dragodindes-card-wrapper')
